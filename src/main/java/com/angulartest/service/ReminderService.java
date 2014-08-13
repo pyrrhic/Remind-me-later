f76161299bf7d70431115d2f2a6f3d1be07cffda
pyrrhic <christopher.patrick.ryan@gmail.com>
2014-08-12 23:02:31 -0400

    umm.. heroku build failed?

---
+++ src/main/java/com/angulartest/service/ReminderService.java
@@ -0,0 +1,37 @@
+package com.angulartest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import com.angulartest.dao.ReminderDAO;
+import com.angulartest.model.Reminder;
+import com.angulartest.model.ReminderFO;
+import com.angulartest.validator.ReminderFOValidator;
+
+@Service	
+public class ReminderService {
+	 
+	private ReminderDAO reminderDAO;
+	
+	@Autowired
+	public ReminderService(ReminderDAO reminderDAO) {
+		this.reminderDAO = reminderDAO;
+	}
+	
+	public void addReminder(ReminderFO reminderFO, BindingResult result) {
+		ReminderFOValidator reminderFOValidator = new ReminderFOValidator();
+		reminderFOValidator.validate(reminderFO, result);
+		
+		if (!result.hasErrors()) {
+			Reminder reminder = reminderFO.convertReminderFOToReminder();
+			
+			if (!reminderDAO.doesReminderExist(reminder)) {
+				reminderDAO.addReminder(reminder, "anonymous@anonymous.com");	
+			}
+			else {
+				System.out.println("A user tried to add the same reminder multiple times. Contact: " + reminder.getCellNumber() + " Msg:" + reminder.getMessage());
+			}
+		}
+	}
+}
