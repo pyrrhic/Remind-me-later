681c676594cda7ae92762a4b1ab59e13bb3d0e42
pyrrhic <christopher.patrick.ryan@gmail.com>
2014-08-29 20:53:36 -0400

    heroku-ize

--- src/main/resources/public/main.js
+++ src/main/resources/public/main.js
@@ -1,3 +1,3 @@
 var myAppModule = angular.module('myApp', []);
 myAppModule.baseUrl = 'http://remindothers.herokuapp.com/';
-myAppModule.baseUrl = 'http://localhost:8090/';
+//myAppModule.baseUrl = 'http://localhost:8090/';
